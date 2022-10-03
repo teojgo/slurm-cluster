3ed0edbd60357061d5d45fd0f6fc72de4f4ecde7
Theofilos Manitaras <manitaras@cscs.ch>
2022-10-03 18:11:56 +0200

    Update running of the reframe tests

--- reframe/docker-entrypoint.sh
+++ reframe/docker-entrypoint.sh
@@ -8,20 +8,12 @@ done
 sudo cp /scratch/munge.key /etc/munge/munge.key
 sudo service munge start
 
-#cd /scratch
-#rm -rf reframe
-#git clone https://github.com/eth-cscs/reframe.git
-#cd reframe
-#./bootstrap.sh
+cd /scratch
+rm -rf reframe
+git clone https://github.com/reframe-hpc/reframe.git
+cd reframe
+./bootstrap.sh
 
-#bash -c "$1"
-#while true
-#do
-#  sleep 1
-#done
-
-#cd /scratch/reframe
-#tempdir=$(mktemp -d -p /scratch)
-#TMPDIR=$tempdir ./test_reframe.py --rfm-user-config=/reframe/container_cluster.py
-
-tail -f /dev/null
+cd /scratch/reframe
+tempdir=$(mktemp -d -p /scratch)
+TMPDIR=$tempdir ./test_reframe.py -v --rfm-user-config=/reframe/container_cluster.py
